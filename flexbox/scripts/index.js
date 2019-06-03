5e557172de965cafc4702e340fba025a77574d2f
its-gio <me@its-g.io>
2019-06-02 21:04:52 -0700

    Added scolling feature to navigation

--- flexbox/scripts/index.js
+++ flexbox/scripts/index.js
@@ -1,3 +1,24 @@
+// Navigation
+let nav = document.querySelector(".nav");
+let navBtns = nav.querySelectorAll(".nav__buttons--button");
+let navHeight = nav.offsetHeight;
+
+function secionScroll(e) {
+  e.preventDefault();
+  let section = e.target.dataset.section;
+  let scrollValue = document.querySelector(`#${section}`).offsetTop - navHeight;
+
+  window.scrollTo({
+    top: scrollValue,
+    left: 0,
+    behavior: "smooth"
+  });
+  return false;
+}
+
+navBtns.forEach(btn => btn.addEventListener("click", secionScroll));
+
+// Footer date
 const date = document.querySelector("#date");
 
 date.textContent = new Date().getFullYear();
